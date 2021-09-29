0bacb7eba88dfbb11a01b0c85a44df595d605d69
nuckarronjones <Scubaa4living@gmail.com>
2021-09-28 21:53:11 -0700

    Revert "Tinkering"
    This reverts commit a77624c9bbf8e08605250bc0cd9d7dac18398273.

--- Doc Editor/Js.js
+++ Doc Editor/Js.js
@@ -1,14 +1,22 @@
-/*
-EXPORT TEXT AREA TO WORD DOCJMENT / DONWLOAD
--https://www.codexworld.com/export-html-to-word-doc-docx-using-javascript/
 
-IDEAS:
--take snapshot of document text every so often for a redo functionality. Maybe after every space so each word itself is saved
+function exportHTML(){//export to word document
+    let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+        "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+        "xmlns='http://www.w3.org/TR/REC-html40'>"+
+        "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+    let footer = "</body></html>";
+    let sourceHTML = header+document.getElementById("text_Container").innerHTML+footer;
+       
+    let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+    let fileDownload = document.createElement("a");
+    document.body.appendChild(fileDownload);
+    fileDownload.href = source;
+    fileDownload.download = `${document.getElementById("documentName").value} .doc`;
+    fileDownload.click();
+    document.body.removeChild(fileDownload);
+}
 
-*/
-
-let colorBoxHidden = true
-let currentOpenBox;
+let activeDropDown;//active dropdown box being used 
 
 let fontList = [
     "Arial",
@@ -29,7 +37,7 @@ let fontList = [
     "Brush Script MT",
     "Luminari",
     "Comic Sans MS"
-]
+].sort();
 
 let colors = [
     ["red","#FF0000"],
@@ -42,23 +50,6 @@ let colors = [
     ["black","#000000"],
     ["white","#FFFFFF"],
 ]
-
-function exportHTML(){//export to word document
-    let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
-        "xmlns:w='urn:schemas-microsoft-com:office:word' "+
-        "xmlns='http://www.w3.org/TR/REC-html40'>"+
-        "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
-    let footer = "</body></html>";
-    let sourceHTML = header+document.getElementById("text_Container").innerHTML+footer;
-       
-    let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
-    let fileDownload = document.createElement("a");
-    document.body.appendChild(fileDownload);
-    fileDownload.href = source;
-    fileDownload.download = `${document.getElementById("documentName").value} .doc`;
-    fileDownload.click();
-    document.body.removeChild(fileDownload);
-}
 
 function printDiv() {//print text area
             let divContents = document.getElementById("text_Container").innerHTML;
@@ -73,17 +64,33 @@ function printDiv() {//print text area
 
 function dropDown(targetId){
 
-    // for(let i = 0; i < document.getElementsByClassName("popup_Selector").length; i++){//this is to help toggle. Existing popup boxes close when another is chosen
-    //     document.getElementsByClassName("popup_Selector")[i].classList.add("hidden")
-    // }//start by hiding all popup boxes
-
-    if(colorBoxHidden){ // viewing a popup box
-        document.getElementById(targetId).classList.remove("hidden")
-        colorBoxHidden = false
+    //clear all visible elements each time a dropdown is selected (start fresh, reset)
+    let toggleElements = document.getElementsByClassName("visible")
+    for(let i = 0; i < toggleElements.length;i++){
+        toggleElements[0].classList.add("hidden")
+        toggleElements[0].classList.remove("visible")
     }
-    else{ // removing a popup box by adding hidden class
+
+    //add visible class to see the popupbox
+    document.getElementById(targetId).classList.remove("hidden")
+    document.getElementById(targetId).classList.add("visible")
+
+
+    if(activeDropDown == targetId){//if the popup box is chosen thats already open, close it
+        document.getElementById(targetId).classList.remove("visible")
         document.getElementById(targetId).classList.add("hidden")
-        colorBoxHidden = true
+
+        activeDropDown = null;//reset active popup box
+    }else{
+        activeDropDown = targetId//set active popup box. Check for this by using the if statement above. If element is already being used. Close it out
+    }
+
+}
+function displayModal(toggle){
+    if(toggle){
+        document.getElementById("main_Modal").classList.remove("hidden")
+    }else if(toggle == false){
+        document.getElementById("main_Modal").classList.add("hidden")
     }
 }
 
@@ -116,12 +123,12 @@ function setLineSpacing(space){
 
 function buildFonts(font){
     for(let i = 0; i < fontList.length; i++){
-        document.getElementById("fontList").innerHTML += `<li onclick='setFont("${fontList[i]}")'> ${fontList[i]} </li>`
+        document.getElementById("fontList").innerHTML += `<li style='font-family: ${fontList[i]};' onclick='setFont("${fontList[i]}")'> ${fontList[i]} &emsp;</li>`
     }
 }
 function buildFontSizes(size){
-    for(let i = 3; i < 75; i++){
-        document.getElementById("font_Sizes").innerHTML += `<li onclick='setFontSize("${i}")'> ${i} </li>`
+    for(let i = 3; i < 76; i++){
+        document.getElementById("font_Sizes").innerHTML += `<li onclick='setFontSize("${i}")'> ${i} &emsp;</li>`
     }
 }
 
