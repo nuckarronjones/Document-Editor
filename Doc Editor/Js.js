7e9b3867e9010e94a8e7dade2e3dc24a0ad4b515
Nicholas Jones <Scubaa4living@gmail.com>
2022-03-03 19:23:31 -0600

    Removed Fonts
    Removed fonts that were unable to be applied. Issues with not applying

--- Doc Editor/Js.js
+++ Doc Editor/Js.js
@@ -10,15 +10,8 @@ let fontList = [
     "Trebuchet MS",
     "Impact",
     "Times New Roman",
-    "Didot",
-    "Georgia",
-    "American Typewriter",
-    "Andalé Mono",
     "Courier",
     "Lucida Console",
-    "Monaco",
-    "Bradley Hand",
-    "Brush Script MT",
     "Luminari",
     "Comic Sans MS"
 ].sort();
